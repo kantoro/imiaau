5e75926a0194f4c8dd04c7920510e83c20a80df3
Kantoro Erkulov <kantoro90@gmail.com>
2012-08-19 15:02:34 +0600

    types

---
+++ src/main/java/kg/edu/iaau/imiaau/types/FriendInfo.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kg.edu.iaau.imiaau.types;
+
+/**
+ *
+ * @author Kantoro
+ */
+public class FriendInfo {
+    
+    public static final String FRIEND_LIST = "friendList";
+    public static final String USERNAME = "username";
+    public static final String IP = "IP";
+    public static final String PORT = "port";
+    public static final String USER_KEY = "userKey";
+    public static final String MESSAGE = "message";  // this should not be in here
+    public static final String STATUS = "status";
+    
+    public STATUS status;
+    public String userName;
+    public String ip;
+    public String port;
+    public String userKey;
+    public String expire;
+
+}
---
+++ src/main/java/kg/edu/iaau/imiaau/types/STATUS.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kg.edu.iaau.imiaau.types;
+
+/**
+ *
+ * @author Kantoro
+ */
+public enum STATUS {
+    ONLINE, OFFLINE, BUSY, INVISIBLE, AWAY, UNAPPROVED
+}
